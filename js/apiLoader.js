8646b0cb291d39ad602e22c8615eb8ee95b848b7
KatePavlovich <asylumhere@mail.ru>
2018-11-19 20:37:05 +0300

    feat: add styles

---
+++ js/apiLoader.js
@@ -0,0 +1,12 @@
+function showMeNews() {
+    var url = 'https://newsapi.org/v2/top-headlines?' +
+        'country=us&' +
+        'apiKey=efe04172a5504a1882090998b0133719';
+    var req = new Request(url);
+    fetch(req)
+        .then(response => response.json())
+        .then((data) => {
+            console.log(data)
+        });
+
+}
